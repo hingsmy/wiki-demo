a41a98af491b7c1c325bed5fe6e7f38f751a1b68
hingsmy <hingsmy@gmail.com>
2021-12-13 18:06:34 +0800

    增加hello接口测试test.http

--- src/main/java/com/hingsmy/wiki/controller/TestController.java
+++ src/main/java/com/hingsmy/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hingsmy.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class TestController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 }
