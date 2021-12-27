b756bfbf8a266d49a66116ff03dc0e2717c46176
hingsmy <hingsmy@gmail.com>
2021-12-27 18:24:09 +0800

    增加通用的返回类CommonResp

--- src/main/java/com/hingsmy/wiki/controller/EbookController.java
+++ src/main/java/com/hingsmy/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.hingsmy.wiki.controller;
 
 import com.hingsmy.wiki.domain.Ebook;
+import com.hingsmy.wiki.resp.CommonResp;
 import com.hingsmy.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
