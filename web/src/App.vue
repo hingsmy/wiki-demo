cb89a97051929c3a5ed8170c8699f032970b3c18
hingsmy <hingsmy@gmail.com>
2021-12-28 10:51:31 +0800

    制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,9 +5,7 @@
 
     <router-view/>
 
-    <a-layout-footer style="text-align: center">
-      Hings电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
 
   </a-layout>
 </template>
@@ -25,11 +23,13 @@
 <script lang="ts">
 import {defineComponent} from "vue";
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
     name: 'app',
     components: {
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   })
 </script>
