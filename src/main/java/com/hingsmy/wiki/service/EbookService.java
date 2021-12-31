e3867d883e6eba2c0d52e42325cc4e1d4b256dff
hingsmy <hingsmy@gmail.com>
2021-12-31 23:20:09 +0800

    集成PageHelper插件，将mapper日志级别改为trace，打印所有的sql日志

--- src/main/java/com/hingsmy/wiki/service/EbookService.java
+++ src/main/java/com/hingsmy/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.hingsmy.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hingsmy.wiki.domain.Ebook;
 import com.hingsmy.wiki.domain.EbookExample;
 import com.hingsmy.wiki.mapper.EbookMapper;
 import com.hingsmy.wiki.req.EbookReq;
 import com.hingsmy.wiki.resp.EbookResp;
 import com.hingsmy.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Autowired
     private EbookMapper ebookMapper;
@@ -25,7 +31,12 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
