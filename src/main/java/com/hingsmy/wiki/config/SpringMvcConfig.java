b3cc8ed01412403d78283b2593edc40ccb63d2ca
hingsmy <hingsmy@gmail.com>
2021-12-31 21:42:25 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/hingsmy/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,19 @@
+package com.hingsmy.wiki.config;
+
+import com.hingsmy.wiki.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
