14af640ea6ebb2993ba3de46ff4c0656b21be229
hingsmy <hingsmy@gmail.com>
2022-01-01 20:00:37 +0800

    增加雪花算法工具类，完成电子书新增功能

--- src/main/java/com/hingsmy/wiki/service/EbookService.java
+++ src/main/java/com/hingsmy/wiki/service/EbookService.java
@@ -10,6 +10,7 @@ import com.hingsmy.wiki.req.EbookSaveReq;
 import com.hingsmy.wiki.resp.EbookQueryResp;
 import com.hingsmy.wiki.resp.PageResp;
 import com.hingsmy.wiki.util.CopyUtil;
+import com.hingsmy.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,9 @@ public class EbookService {
 
     @Autowired
     private EbookMapper ebookMapper;
+
+    @Autowired
+    private SnowFlake snowFlake;
 
     public PageResp<EbookQueryResp> list(EbookQueryReq req) {
 
@@ -63,6 +67,7 @@ public class EbookService {
         Ebook ebook = CopyUtil.copy(req, Ebook.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             // 新增
+            ebook.setId(snowFlake.nextId());
             ebookMapper.insert(ebook);
         } else {
             // 更新
